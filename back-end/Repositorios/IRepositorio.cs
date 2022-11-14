d15c9e2835d549dcea52b629bf6ca93dc7969979
Nico <nicoyoles@gmail.com>
2022-11-14 14:36:29 -0300

    merge 71 1436 14112022

---
+++ back-end/Repositorios/IRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using back_end.Entidades;
+using System.Collections.Generic;
+
+namespace back_end.Repositorios
+{
+    public interface IRepositorio
+    {
+        List<Genero> ObtenerTodosLoSGeneros();
+    }
+}
---
+++ back-end/Repositorios/RepositorioEnMemoria.cs
@@ -0,0 +1,24 @@
+﻿using back_end.Entidades;
+using System.Collections.Generic;
+
+namespace back_end.Repositorios
+{
+    public class RepositorioEnMemoria: IRepositorio
+    {
+        private List<Genero> _generos;
+
+        public RepositorioEnMemoria()
+        {
+            _generos = new List<Genero>()
+            {
+                new Genero(){ Id = 1, Nombre = "Comedia"},
+                new Genero(){ Id = 2, Nombre = "Accion"}
+            };
+        }
+
+        public List<Genero> ObtenerTodosLoSGeneros()
+        {
+            return _generos;
+        }
+    }
+}
