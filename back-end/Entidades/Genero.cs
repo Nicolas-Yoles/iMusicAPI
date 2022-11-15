ad931297cde37dbddaa85c764e7124bb04b9dfcd
Nico <nicoyoles@gmail.com>
2022-11-15 13:16:43 -0300

    merge  78 1316 15112022

--- back-end/Entidades/Genero.cs
+++ back-end/Entidades/Genero.cs
@@ -1,14 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using back_end.Validaciones;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace back_end.Entidades
 {
-    public class Genero
+    public class Genero: IValidatableObject
     {
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 10, ErrorMessage = "La cantidad maxima de caracteres debe ser igual a 10")]
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula",
+                        new string[] { nameof(Nombre) });
+                }
+            }
+        }
 
         //[Range(18,20)]
         //public int Edad { get; set; }
