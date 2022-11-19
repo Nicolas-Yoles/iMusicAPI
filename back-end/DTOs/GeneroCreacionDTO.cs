b9a659a534a60c1024d9cb0c80b31c140a9bee72
Nico <nicoyoles@gmail.com>
2022-11-19 11:26:45 -0300

    93 1126 19112022

---
+++ back-end/DTOs/GeneroCreacionDTO.cs
@@ -0,0 +1,13 @@
+﻿using back_end.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace back_end.DTOs
+{
+    public class GeneroCreacionDTO
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, ErrorMessage = "El nombre no debe ser mayor a 10 caracteres")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+    }
+}
---
+++ back-end/DTOs/GeneroDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace back_end.DTOs
+{
+    public class GeneroDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
+
+ 
